ee812d230ae14f90ceaa747f903b267356d49dc9
b01tech <bruno01.tech@gmail.com>
2026-01-30 10:32:09 -0300

    feat(user): add UserName value object

---
+++ src/Core/ValueObjects/UserName.cs
@@ -0,0 +1,25 @@
+﻿using Core.Errors;
+using Core.Results;
+
+namespace Core.ValueObjects;
+
+public class UserName
+{
+    public string Value { get; }
+    private const uint MinLenght = 3;
+
+    private UserName(string value)
+    {
+        Value = value;
+    }
+
+    public static Result<UserName> Create(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            return Result<UserName>.Failure(ErrorMessagesResource.NAME_EMPTY);
+        if (value.Length < MinLenght)
+            return Result<UserName>.Failure(ErrorMessagesResource.NAME_INVALID);
+
+        return new UserName(value);
+    }
+}
