7935c1f22b1ef7593bfd5f85e74966bd9c412413
b01tech <bruno01.tech@gmail.com>
2026-01-30 17:24:52 -0300

    refactor(user): extract user validation to separate service

--- src/Application/UseCases/User/RegisterUserUseCase.cs
+++ src/Application/UseCases/User/RegisterUserUseCase.cs
@@ -4,22 +4,20 @@ using Application.Dtos.Responses;
 using Application.Services.Encrypter;
 using Application.UseCases.User.Interfaces;
 using Core.Common.Repositories;
-using Core.Errors;
 using Core.Results;
-using Core.ValueObjects;
 
 namespace Application.UseCases.User;
 
 internal class RegisterUserUseCase(
     IUserWriteRepository writeRepository,
-    IUserReadOnlyRepository readOnlyRepository,
+    IUserValidator validator,
     IUnitOfWork unitOfWork,
     IEncrypter encrypter)
     : IRegisterUserUseCase
 {
     public async Task<Result<UserResponse>> ExecuteAsync(RegisterUserRequest request)
     {
-        var credentialsResult = await ValidateRequestCredentialsAsync(request);
+        var credentialsResult = await validator.ValidateRequestCredentialsAsync(request);
 
         if (!credentialsResult.IsSuccess)
             return Result<UserResponse>.Failure(credentialsResult.ErrorResponse.Errors,
@@ -34,30 +32,6 @@ internal class RegisterUserUseCase(
         var user = await writeRepository.RegisterUser(userResult.Data!);
         await unitOfWork.CommitAsync();
 
-        return new UserResponse(user.Data.Username.Value);
-    }
-
-    private async Task<Result<(Password password, UserName userName)>> ValidateRequestCredentialsAsync(
-        RegisterUserRequest request)
-    {
-        var passwordResult = Password.Create(request.Password);
-        var userNameResult = UserName.Create(request.Name);
-
-        var errors = new List<string>();
-        if (!userNameResult.IsSuccess)
-            errors.AddRange(userNameResult.ErrorResponse.Errors);
-
-        if (!passwordResult.IsSuccess)
-            errors.AddRange(passwordResult.ErrorResponse.Errors);
-
-        if (errors.Count > 0)
-            return Result<(Password password, UserName userName)>.Failure(errors, (int)HttpStatusCode.BadRequest);
-
-        var userExists = await readOnlyRepository.UserExists(userNameResult.Data!.Value);
-
-        if (!userExists) return (passwordResult.Data!, userNameResult.Data!);
-
-        errors.Add(ErrorMessagesResource.USER_ALREADY_EXISTS);
-        return Result<(Password password, UserName userName)>.Failure(errors, (int)HttpStatusCode.Conflict);
+        return new UserResponse(user.Data!.Username.Value);
     }
 }
