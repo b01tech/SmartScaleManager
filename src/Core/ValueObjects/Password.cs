b21bd11f76439113e404dc600de7fbd1475f4bdb
b01tech <bruno01.tech@gmail.com>
2026-01-30 15:34:03 -0300

    feat: add Password value object

---
+++ src/Core/ValueObjects/Password.cs
@@ -0,0 +1,22 @@
+﻿using Core.Errors;
+using Core.Results;
+
+namespace Core.ValueObjects;
+
+public class Password
+{
+    public string Value { get; private set; } = string.Empty;
+    private const uint MinLenght = 6;
+
+    public Password(string value)
+    {
+        Value = value;
+    }
+
+    public static Result<Password> Create(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value) || value.Length < MinLenght)
+            return Result<Password>.Failure(ErrorMessagesResource.PASSWORD_INVALID);
+        return Result<Password>.Success(new Password(value));
+    }
+}
