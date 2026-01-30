f0dcee238c1790e012942001ed1c9b81164861c8
b01tech <bruno01.tech@gmail.com>
2026-01-30 14:17:05 -0300

    feat(infra): add User entity mapping configuration

---
+++ src/Infra/Mapping/UserMap.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Mapping;
+
+public static class UserMap
+{
+    public static void Configure(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.ToTable("users");
+            entity.HasKey(x => x.Id);
+            entity.Property(x => x.Id).ValueGeneratedOnAdd();
+            entity.Property(x => x.Username).IsRequired().HasMaxLength(50);
+            entity.Property(x => x.HashPassword).IsRequired().HasMaxLength(255);
+            entity.Property(x => x.IsActive).IsRequired();
+            entity.Property(x => x.Role).IsRequired();
+        });
+    }
+}
