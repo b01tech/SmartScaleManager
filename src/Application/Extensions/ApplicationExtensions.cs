e53915859e3ac57f533abb872560c0d94bfd3f43
b01tech <bruno01.tech@gmail.com>
2026-01-30 13:53:37 -0300

    feat: add application service registration extension

---
+++ src/Application/Extensions/ApplicationExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Extensions;
+
+public static class ApplicationExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddUseCases();
+        return services;
+    }
+}
