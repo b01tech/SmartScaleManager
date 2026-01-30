1f419d366ef70aec531d46b00cb95701bd8ff1f7
b01tech <bruno01.tech@gmail.com>
2026-01-30 14:23:48 -0300

    feat: add UserRepository

---
+++ src/Infra/Repositories/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Core.Common.Repositories;
+using Core.Entities;
+using Core.Results;
+using Infra.Data;
+
+namespace Infra.Repositories;
+
+internal class UserRepository(ScaleAppDbContext dbContext) : IUserWriteRepository
+{
+    public async Task<Result<User>> RegisterUser(User user)
+    {
+        await dbContext.Users.AddAsync(user);
+        return Result<User>.Success(user);
+    }
+}
