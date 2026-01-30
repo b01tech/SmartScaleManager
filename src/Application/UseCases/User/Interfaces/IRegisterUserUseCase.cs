9dd361020651821795fd9d4041ef49cdfe8c1f24
b01tech <bruno01.tech@gmail.com>
2026-01-30 13:52:56 -0300

    feat(user): add user registration use case

---
+++ src/Application/UseCases/User/Interfaces/IRegisterUserUseCase.cs
@@ -0,0 +1,10 @@
+﻿using Application.Dtos.Requests;
+using Application.Dtos.Responses;
+using Core.Results;
+
+namespace Application.UseCases.User.Interfaces;
+
+public interface IRegisterUserUseCase
+{
+    Task<Result<UserResponse>> ExecuteAsync(RegisterUserRequest request);
+}
---
+++ src/Application/UseCases/User/RegisterUserUseCase.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using Application.Dtos.Requests;
+using Application.Dtos.Responses;
+using Application.UseCases.User.Interfaces;
+using Core.Results;
+
+namespace Application.UseCases.User;
+
+internal class RegisterUserUseCase : IRegisterUserUseCase
+{
+    public Task<Result<UserResponse>> ExecuteAsync(RegisterUserRequest request)
+    {
+        var userResult = Core.Entities.User.Create(name: request.Name, hashPassword: request.Password, role: request.Role);
+        if (!userResult.IsSuccess)
+            return Task.FromResult(Result<UserResponse>.Failure(userResult.ErrorResponse.Errors, (int)HttpStatusCode.BadRequest));
+        var user = userResult.Data;
+        return Task.FromResult<Result<UserResponse>>(new UserResponse(user!.Username.Value));
+    }
+}
