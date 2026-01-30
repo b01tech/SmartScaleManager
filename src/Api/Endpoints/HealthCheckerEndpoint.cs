a0bb1d76e73534b3c5af0956ce98f137bbc60baf
b01tech <bruno01.tech@gmail.com>
2026-01-30 13:52:01 -0300

    docs(api): add tags and summary to health endpoint

--- src/Api/Endpoints/HealthCheckerEndpoint.cs
+++ src/Api/Endpoints/HealthCheckerEndpoint.cs
@@ -5,6 +5,8 @@ public static class HealthCheckerEndpoint
     public static void Map(IEndpointRouteBuilder app)
     {
         app.MapGet("/health", () => new { status = "OK" })
-            .WithName("HealthChecker");
+            .WithName("HealthChecker")
+            .WithTags("Health Checker")
+            .WithSummary("Checks the health of the application");
     }
 }
