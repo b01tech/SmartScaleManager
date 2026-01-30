ceb00c3aeaab596591b69e01f78ec284fb8dd695
b01tech <bruno01.tech@gmail.com>
2026-01-30 11:18:04 -0300

    feat(api): add OpenAPI documentation with Scalar UI

---
+++ src/Api/Extensions/DocumentationExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Scalar.AspNetCore;
+
+namespace Api.Extensions;
+
+public static class DocumentationExtensions
+{
+    public static IServiceCollection AddDocumentationApi(this IServiceCollection services)
+    {
+        services.AddOpenApi();
+        return services;
+    }
+
+    public static void UseDocumentationApi(this IEndpointRouteBuilder app)
+    {
+        app.MapOpenApi();
+        app.MapScalarApiReference("/docs",options =>
+        {
+           options.Title = "Smart Scale Manager API";
+           options.Theme = ScalarTheme.Solarized;
+        });
+    }
+}
