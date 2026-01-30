89de9b02089f25ce5a5c6bd24cd4c916a32868d3
b01tech <bruno01.tech@gmail.com>
2026-01-30 17:10:13 -0300

    feat(application): add AppServices to dependency injection
    Register application services via AddAppServices met

---
+++ src/Application/Extensions/AppServiceExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Application.Services.Encrypter;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Extensions;
+
+public static class AppServiceExtensions
+{
+    public static void AddAppServices(this IServiceCollection services)
+    {
+        services.AddScoped<IEncrypter, Encrypter>();
+    }
+}
--- src/Application/Extensions/ApplicationExtensions.cs
+++ src/Application/Extensions/ApplicationExtensions.cs
@@ -7,6 +7,7 @@ public static class ApplicationExtensions
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddUseCases();
+        services.AddAppServices();
         return services;
     }
 }
