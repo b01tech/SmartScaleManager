b125e3aae65b6a447776e294877c4fb86b1f1429
b01tech <bruno01.tech@gmail.com>
2026-01-30 17:09:11 -0300

    feat(encryption): add encrypter interface and implementation

---
+++ src/Application/Services/Encrypter/Encrypter.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Services.Encrypter;
+
+internal class Encrypter : IEncrypter
+{
+    public string Encrypt(string password) =>
+        BCrypt.Net.BCrypt.HashPassword(password);
+
+    public bool Verify(string password, string hashedPassword) =>
+        BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+}
---
+++ src/Application/Services/Encrypter/IEncrypter.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Services.Encrypter;
+
+public interface IEncrypter
+{
+    string Encrypt(string password);
+    bool Verify(string password, string hashedPassword);
+}
