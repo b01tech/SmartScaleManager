98d322b532b90c032470eb9a899c446310b7e3a2
b01tech <bruno01.tech@gmail.com>
2026-01-30 11:18:18 -0300

    feat: add API documentation

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,9 +1,11 @@
 using Api.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDocumentationApi();
 
 var app = builder.Build();
 
+app.UseDocumentationApi();
 app.UseHttpsRedirection();
 app.UseAppEndpoints();
 app.Run();
