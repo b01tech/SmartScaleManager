8b001e10b0f214523954326bcc92830056778f9a
b01tech <bruno01.tech@gmail.com>
2026-01-30 17:09:50 -0300

    feat(repository): implement user existence check in UserRepository

--- src/Infra/Repositories/UserRepository.cs
+++ src/Infra/Repositories/UserRepository.cs
@@ -2,14 +2,20 @@
 using Core.Entities;
 using Core.Results;
 using Infra.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infra.Repositories;
 
-internal class UserRepository(ScaleAppDbContext dbContext) : IUserWriteRepository
+internal class UserRepository(ScaleAppDbContext dbContext) : IUserWriteRepository, IUserReadOnlyRepository
 {
     public async Task<Result<User>> RegisterUser(User user)
     {
         await dbContext.Users.AddAsync(user);
         return Result<User>.Success(user);
+    }
+
+    public async Task<bool> UserExists(string username)
+    {
+        return await dbContext.Users.AnyAsync(u => u.Username.Value.Equals(username));
     }
 }
