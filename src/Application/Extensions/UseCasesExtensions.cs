4da6b9b867e932c13436a20b5cf62c2843a8ddbb
b01tech <bruno01.tech@gmail.com>
2026-01-30 13:53:19 -0300

    build: add dependency injection abstractions package

---
+++ src/Application/Extensions/UseCasesExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Application.UseCases.User;
+using Application.UseCases.User.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Extensions;
+
+public static class UseCasesExtensions
+{
+    internal static IServiceCollection AddUseCases(this IServiceCollection services)
+    {
+        services.AddScoped<IRegisterUserUseCase, RegisterUserUseCase>();
+        return services;
+    }
+}
