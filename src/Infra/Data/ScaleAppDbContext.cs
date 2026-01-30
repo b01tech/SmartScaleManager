a98d04b1f184ad08ab17aa32a280d428c5c77fca
b01tech <bruno01.tech@gmail.com>
2026-01-30 14:17:26 -0300

    feat(infra): add ScaleAppDbContext

---
+++ src/Infra/Data/ScaleAppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities;
+using Infra.Mapping;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Data;
+
+public class ScaleAppDbContext(DbContextOptions<ScaleAppDbContext> options) : DbContext(options)
+{
+    public DbSet<User> Users { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        UserMap.Configure(modelBuilder);
+    }
+}
