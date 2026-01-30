1f6983dac37b878b81b32c73e489c979896c27a9
b01tech <bruno01.tech@gmail.com>
2026-01-30 10:47:17 -0300

    feat: add health check endpoint via extension method

---
+++ src/Api/Extensions/EndpointExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Api.Endpoints;
+
+namespace Api.Extensions;
+
+public static class EndpointExtensions
+{
+    public static void UseAppEndpoints(this IEndpointRouteBuilder app)
+    {
+        HealthCheckerEndpoint.Map(app);
+    }
+}
--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,7 +1,9 @@
+using Api.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
-
+app.UseAppEndpoints();
 app.Run();
