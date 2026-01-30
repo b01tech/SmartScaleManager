a514e00011464413c1f53f9d80a2f02e642b84f7
b01tech <bruno01.tech@gmail.com>
2026-01-30 14:17:41 -0300

    feat(infra): add infrastructure and DbContext service extensions

---
+++ src/Infra/Extensions/DbContextExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Infra.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infra.Extensions;
+
+public static class DbContextExtensions
+{
+    public static void AddContext(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection") ?? string.Empty;
+        services.AddDbContext<ScaleAppDbContext>(options =>
+            options.UseNpgsql(connectionString));
+    }
+}
---
+++ src/Infra/Extensions/InfrastructureExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infra.Extensions;
+
+public static class InfrastructureExtensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddContext(configuration);
+        return services;
+    }
+}
