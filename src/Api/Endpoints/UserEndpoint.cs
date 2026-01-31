59aab692831aae8786402174d55b1e70fe1940e4
b01tech <bruno01.tech@gmail.com>
2026-01-31 09:44:19 -0300

    feat(user): add user endpoints for get, activate, and remove

--- src/Api/Endpoints/UserEndpoint.cs
+++ src/Api/Endpoints/UserEndpoint.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Dtos.Requests;
 using Application.Dtos.Responses;
 using Application.UseCases.User.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Endpoints;
 
@@ -10,18 +11,46 @@ public static class UserEndpoint
     {
         var group = app.MapGroup("users").WithTags("Users");
 
-        group.MapPost("/register", async (RegisterUserRequest request, IRegisterUserUseCase useCase) =>
-            {
-                var result = await useCase.ExecuteAsync(request);
-                if (result.IsSuccess)
-                    return Results.Created(string.Empty, result.Data);
+        group.MapPost("/register",
+                async ([FromBody] RegisterUserRequest request, [FromServices] IRegisterUserUseCase useCase) =>
+                {
+                    var result = await useCase.ExecuteAsync(request);
+                    if (result.IsSuccess)
+                        return Results.Created(string.Empty, result.Data);
 
-                return (result.ErrorResponse.Status == StatusCodes.Status409Conflict)
-                    ? Results.Conflict(result.ErrorResponse)
-                    : Results.BadRequest(result.ErrorResponse);
-
-            }).WithName("RegisterUser")
+                    return (result.ErrorResponse.Status == StatusCodes.Status409Conflict)
+                        ? Results.Conflict(result.ErrorResponse)
+                        : Results.BadRequest(result.ErrorResponse);
+                }).WithName("RegisterUser")
             .WithSummary("Registers a user")
             .Produces<UserResponse>(StatusCodes.Status201Created);
+
+        group.MapGet("/{id:long}", async ([FromRoute] long id, [FromServices] IGetUserUseCase useCase) =>
+            {
+                var result = await useCase.ExecuteAsync(id);
+                if (result.IsSuccess)
+                    return Results.Ok(result.Data);
+                return (result.ErrorResponse.Status == StatusCodes.Status404NotFound)
+                    ? Results.NotFound(result.ErrorResponse)
+                    : Results.BadRequest(result.ErrorResponse);
+            }).WithName("GetUserDetails")
+            .WithSummary("Retrieved a user by Id")
+            .Produces<UserDetailsResponse>(StatusCodes.Status200OK);
+
+        group.MapPost("/active/{id:long}", async ([FromRoute] long id, [FromServices] IActiveUserUseCase useCase) =>
+            {
+                var result = await useCase.ExecuteAsync(id);
+                return result.IsSuccess ? Results.NoContent() : Results.BadRequest(result.ErrorResponse);
+            }).WithName("ActivateUser")
+            .WithSummary("Activates a user")
+            .Produces(StatusCodes.Status204NoContent);
+
+        group.MapDelete("/remove/{id:long}", async ([FromRoute] long id, [FromServices] IRemoveUserUseCase useCase) =>
+            {
+                var result = await useCase.ExecuteAsync(id);
+                return result.IsSuccess ? Results.NoContent() : Results.BadRequest(result.ErrorResponse);
+            }).WithName("RemoveUser")
+            .WithSummary("Removes a user")
+            .Produces(StatusCodes.Status204NoContent);
     }
 }
