d49c63dd84fa7ebb6d54bcc1ea84cdfcdb14126d
b01tech <bruno01.tech@gmail.com>
2026-01-31 09:43:41 -0300

    feat: add user use cases to services di

--- src/Application/Extensions/UseCasesExtensions.cs
+++ src/Application/Extensions/UseCasesExtensions.cs
@@ -9,6 +9,10 @@ public static class UseCasesExtensions
     internal static IServiceCollection AddUseCases(this IServiceCollection services)
     {
         services.AddScoped<IRegisterUserUseCase, RegisterUserUseCase>();
+        services.AddScoped<IActiveUserUseCase, ActiveUserUseCase>();
+        services.AddScoped<IRemoveUserUseCase, RemoveUserUseCase>();
+        services.AddScoped<IGetUserUseCase, GetUserUseCase>();
+
         return services;
     }
 }
