2f87657bdc9185cec44272ab54efffbe0fff7cf8
b01tech <bruno01.tech@gmail.com>
2026-01-31 09:42:24 -0300

    feat(user): add soft delete, activate and get by id methods implementation

--- src/Infra/Repositories/UserRepository.cs
+++ src/Infra/Repositories/UserRepository.cs
@@ -14,8 +14,30 @@ internal class UserRepository(ScaleAppDbContext dbContext) : IUserWriteRepositor
         return Result<User>.Success(user);
     }
 
+    public async Task SoftDelete(long id)
+    {
+        var user = await dbContext.Users.FindAsync(id);
+        user?.Deactivate();
+    }
+
+    public async Task Activate(long id)
+    {
+        var user = await dbContext.Users.FindAsync(id);
+        user?.Activate();
+    }
+
     public async Task<bool> UserExists(string username)
     {
         return await dbContext.Users.AnyAsync(u => u.Username.Value.Equals(username));
+    }
+
+    public async Task<bool> UserExists(long id)
+    {
+        return await dbContext.Users.AnyAsync(u => u.Id.Equals(id));
+    }
+
+    public async Task<User?> GetById(long id)
+    {
+        return await dbContext.Users.FindAsync(id);
     }
 }
