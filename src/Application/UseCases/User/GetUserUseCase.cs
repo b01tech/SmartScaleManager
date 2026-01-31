7b0479e9728125c4e4c46919d572a520f0859e1a
b01tech <bruno01.tech@gmail.com>
2026-01-31 09:42:43 -0300

    feat(user): add getUser use case with interface and implementation

---
+++ src/Application/UseCases/User/GetUserUseCase.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using Application.Dtos.Requests;
+using Application.Dtos.Responses;
+using Application.UseCases.User.Interfaces;
+using Core.Common.Repositories;
+using Core.Errors;
+using Core.Results;
+
+namespace Application.UseCases.User;
+
+internal class GetUserUseCase(IUserReadOnlyRepository repository) : IGetUserUseCase
+{
+    public async Task<Result<UserDetailsResponse>> ExecuteAsync(long id)
+    {
+        var user = await repository.GetById(id);
+        if (user is null)
+            return Result<UserDetailsResponse>.Failure(ErrorMessagesResource.USER_NOT_FOUND,
+                (int)HttpStatusCode.NotFound);
+        return new UserDetailsResponse(
+            Id: user.Id,
+            Name: user.Username.Value,
+            Role: user.Role.ToString(),
+            IsActive: user.IsActive);
+    }
+}
---
+++ src/Application/UseCases/User/Interfaces/IGetUserUseCase.cs
@@ -0,0 +1,10 @@
+﻿using Application.Dtos.Requests;
+using Application.Dtos.Responses;
+using Core.Results;
+
+namespace Application.UseCases.User.Interfaces;
+
+public interface IGetUserUseCase
+{
+    Task<Result<UserDetailsResponse>> ExecuteAsync(long id);
+}
