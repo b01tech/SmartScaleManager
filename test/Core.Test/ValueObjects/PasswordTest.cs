3edd9d87119481b08a413bfef5a54f71233ca55b
b01tech <bruno01.tech@gmail.com>
2026-02-01 17:47:09 -0300

    test: add unit tests for Password value object

---
+++ test/Core.Test/ValueObjects/PasswordTest.cs
@@ -0,0 +1,37 @@
+﻿using Core.ValueObjects;
+
+namespace Core.Test.ValueObjects;
+
+public class PasswordTest
+{
+    [Fact]
+    public void Create_ValidPassword_Success()
+    {
+        // Arrange
+        const string input = "123456";
+        // Act
+        var result = Password.Create(input);
+        //Assert
+        Assert.NotNull(result);
+        Assert.True(result.IsSuccess);
+        Assert.Equal(input, result.Data!.Value);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("      ")]
+    [InlineData("a")]
+    [InlineData("   a    ")]
+    [InlineData("aa")]
+    [InlineData("aaa")]
+    [InlineData("aaaa")]
+    [InlineData("aaaaa")]
+    public void Create_InvalidPassword_Success(string input)
+    {
+        // Arrange & Act
+        var result = Password.Create(input);
+        // Assert
+        Assert.NotNull(result);
+        Assert.False(result.IsSuccess);
+    }
+}
